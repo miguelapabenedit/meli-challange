752af406ee0018198792b24c264fa53de9c58a6a
Miguel Apa Benedit <miguelapabenedit@gmail.com>
2021-01-10 22:01:23 -0300

    Fix Test

--- internal/algorithm/decode-algorithm_test.go
+++ internal/algorithm/decode-algorithm_test.go
@@ -1,56 +1,68 @@
 package algorithm
 
-// func TestProcessMessages_WithRequiredLenghtData_ReturnMessage(t *testing.T) {
-// 	type test struct {
-// 		dataMsg1 []string
-// 		dataMsg2 []string
-// 		dataMsg3 []string
-// 		answer   string
-// 		err      error
-// 	}
+import (
+	"testing"
 
-// 	testcases := []test{
-// 		{
-// 			dataMsg1: []string{"0", "1", "", ""},
-// 			dataMsg2: []string{"", "", "2", ""},
-// 			dataMsg3: []string{"", "1", "", "3"},
-// 			answer:   "0 1 2 3 ",
-// 			err:      nil,
-// 		},
-// 		{
-// 			dataMsg1: []string{"", "", "", ""},
-// 			dataMsg2: []string{"", "1", "2", "3"},
-// 			dataMsg3: []string{"0", "1", "", "3"},
-// 			answer:   "0 1 2 3 ",
-// 			err:      nil,
-// 		},
-// 	}
+	"github.com/stretchr/testify/assert"
+)
 
-// 	for _, val := range testcases {
+func TestProcessMessages_WithRequiredLenghtData_ReturnMessage(t *testing.T) {
+	type test struct {
+		msgs   [][]string
+		answer string
+		err    error
+	}
 
-// 		got, err := ProcessMessages(val.dataMsg1, val.dataMsg2, val.dataMsg3)
+	testcases := []test{
+		{
+			msgs: [][]string{
+				{"0", "1", "", ""},
+				{"", "", "2", ""},
+				{"", "", "", "3"},
+			},
+			answer: "0 1 2 3 ",
+			err:    nil,
+		},
+		{
+			msgs: [][]string{
+				{"", "", "", ""},
+				{"", "1", "2", "3"},
+				{"0", "1", "", "3"},
+			},
+			answer: "0 1 2 3 ",
+			err:    nil,
+		},
+	}
 
-// 		if val.err != nil {
-// 			assert.Error(t, err, val.err)
-// 		}
-// 		assert.Equal(t, val.answer, got)
-// 	}
-// }
+	for _, val := range testcases {
 
-// func TestProcessMessages_InvalidAmountOfArrays_ReturnError(t *testing.T) {
-// 	msgData := []string{"", "", ""}
-// 	got, err := ProcessMessages(msgData)
+		got, err := ProcessMessages(val.msgs)
 
-// 	assert.Error(t, err, "Expected array lenght to be > 1 is required")
-// 	assert.Equal(t, "", got)
-// }
+		if val.err != nil {
+			assert.Error(t, err, val.err)
+		}
+		assert.Equal(t, val.answer, got)
+	}
+}
 
-// func TestProcessMessages_EmtyArrays_ReturnError(t *testing.T) {
-// 	msgData1 := []string{"", "", ""}
-// 	msgData2 := []string{"", "", ""}
-// 	msgData3 := []string{"", "", ""}
-// 	got, err := ProcessMessages(msgData1, msgData2, msgData3)
+func TestProcessMessages_InvalidAmountOfArrays_ReturnError(t *testing.T) {
+	msgData := [][]string{
+		{"0", "1", "2", "3"},
+	}
+	got, err := ProcessMessages(msgData)
 
-// 	assert.Error(t, err, "Unable to determine the message")
-// 	assert.Equal(t, "", got)
-// }
+	assert.Error(t, err, "Expected array lenght to be > 1 is required")
+	assert.Equal(t, "", got)
+}
+
+func TestProcessMessages_EmtyArrays_ReturnError(t *testing.T) {
+	msgData := [][]string{
+		{"", "", "", ""},
+		{"", "", "", ""},
+		{"", "", "", ""},
+	}
+	got, err := ProcessMessages(msgData)
+
+	assert.Error(t, err, "Unable to determine the message")
+	assert.Equal(t, "", got)
+}
